987333d29227008ec4b8e7714addf774962f2a30
Dexter.in <31876122+kusha-code@users.noreply.github.com>
2022-10-09 21:57:41 +0530

    Create givenFormat.java

---
+++ givenFormat.java
@@ -0,0 +1,15 @@
+// important import statements  
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
+    
+public class CurrentDateTimeExample7   
+{      
+  // main method  
+  public static void main(String[] argvs)   
+  {      
+    // creating a new object of the class Date  
+    java.util.Date date = new java.util.Date();    
+    System.out.println(date);   
+       
+  }      
+}  
