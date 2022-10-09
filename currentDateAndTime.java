b04610a4046c4d2a82808825986bc70d55ebfc5a
Dexter.in <31876122+kusha-code@users.noreply.github.com>
2022-10-09 21:52:25 +0530

    Create currentDateAndTime.java

---
+++ currentDateAndTime.java
@@ -0,0 +1,12 @@
+// important import statements  
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
+    
+public class CurrentDateTimeExample4   
+{      
+  // main method  
+  public static void main(String[] argvs)   
+  {      
+        System.out.println(java.time.LocalTime.now());    
+  }      
+}  
